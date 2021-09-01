283c653137785e7af0794638d6bcd7c1681b180d
junchikang <junchikang0611@163.com>
2021-09-01 16:46:24 +0800

    更新函数

---
+++ djangoProject/README.md
@@ -0,0 +1,12 @@
+# 此代码是作者暑期项目的后端部分
+
+> 使用Django框架，储存数据及函数
+
+## 简易指令示例
+
+``` bash
+# 运行项目
+    python manage.py runserver
+```
+若有其他问题请联系作者康俊驰（junchikang0611@163.com）
+### :)
--- djangoProject/backend/views.py
+++ djangoProject/backend/views.py
@@ -12,7 +12,7 @@ import numpy as np
 
 
 def hello(request):
-    return JsonResponse({"status": 0, "message": "This is a message from Django"})  # 返回json数据类型
+    return JsonResponse({"status": 0, "message": "欢迎使用本项目！"})  # 返回json数据类型
 
 
 def load(request):
@@ -59,14 +59,14 @@ def testsimilar(request):
 def index(request):
     return render(request, 'index.html')
 
-# # 获取的是前端的ID和label,返回的是一个列表
+# 获取的是前端的ID和label,返回的是一个列表
 # def mostsimilar(request):
 #     if request.method == "POST":
 #         files = open('programme_file/JHotDraw.files', 'r')
 #         clones = open('programme_file/JHotDraw.clones', 'r')
 #         name = files.readlines()
 #         an1 = 0
-#         an2 = 0  # 分别为两个文件开头注释的行数
+#         an2 = 0    # 分别为两个文件开头注释的行数
 #         for item in name:
 #             if item[0] != '#':
 #                 break
@@ -74,8 +74,8 @@ def index(request):
 #         line1 = len(name) - an1  # 为.files文件除注释外的行数
 #         namelist = []
 #         for i in range(0, line1):
-#             name[an1 + i] = name[an1 + i].split()
-#             namelist.append(name[an1 + i][1])  # 列表中是每个文件序号对应的文件名
+#             name[an1+i] = name[an1+i].split()
+#             namelist.append(name[an1+i][1])   # 列表中是每个文件序号对应的文件名
 #         similarlines = clones.readlines()
 #         for item in similarlines:
 #             if item[0] != '#':
@@ -85,25 +85,21 @@ def index(request):
 #         array1 = np.zeros((line1, line1))  # 二维列表记录x文件与y文件相似的行数中x文件的行数之和
 #         array2 = np.zeros((line1, line1))  # 二维列表记录x文件与y文件相似的行数中y文件的行数之和
 #         for i in range(0, line2):
-#             similarlines[an2 + i] = similarlines[an2 + i].split(',')
-#             array1[int(similarlines[an2 + i][0])][int(similarlines[an2 + i][3])] += (
-#                     int(similarlines[an2 + i][2]) - int(similarlines[an2 + i][1]))
-#             array2[int(similarlines[an2 + i][0])][int(similarlines[an2 + i][3])] += (
-#                     int(similarlines[an2 + i][5]) - int(similarlines[an2 + i][4]))
-#             array1[int(similarlines[an2 + i][3])][int(similarlines[an2 + i][0])] += (
-#                     int(similarlines[an2 + i][5]) - int(similarlines[an2 + i][4]))
-#             array2[int(similarlines[an2 + i][3])][int(similarlines[an2 + i][0])] += (
-#                     int(similarlines[an2 + i][2]) - int(similarlines[an2 + i][1]))
+#             similarlines[an2+i] = similarlines[an2+i].split(',')
+#             array1[int(similarlines[an2+i][0])][int(similarlines[an2+i][3])] += (int(similarlines[an2+i][2]) - int(similarlines[an2+i][1]))
+#             array2[int(similarlines[an2+i][0])][int(similarlines[an2+i][3])] += (int(similarlines[an2+i][5]) - int(similarlines[an2+i][4]))
+#             array1[int(similarlines[an2+i][3])][int(similarlines[an2+i][0])] += (int(similarlines[an2+i][5]) - int(similarlines[an2+i][4]))
+#             array2[int(similarlines[an2+i][3])][int(similarlines[an2+i][0])] += (int(similarlines[an2+i][2]) - int(similarlines[an2+i][1]))
 #         # 对两个二维数组进行赋值
 #         name = request.POST.get('id')
 #         num = request.POST.get('label')
 #         sortedlist = []
 #         for i in range(0, line1):
 #             if array2[name][i] != 0:
-#                 sortedlist.append(array1[name][i] + 1 / array2[name][i])
+#                 sortedlist.append(array1[name][i] + 1 - 1/array2[name][i])
 #             else:
 #                 sortedlist.append(array1[name][i])
-#                 # 该列表中记录的是选取的文件与所有文件的相似度的数值表征
+#         # 该列表中记录的是选取的文件与所有文件的相似度的数值表征
 #         sortedlist[name] = 0  # 去除该文件本身
 #         sortedlist = np.array(sortedlist)
 #         arglist = np.argsort(-sortedlist)  # 该列表为sortedlist进行从大到小排序后原数字下标的顺序
@@ -111,19 +107,19 @@ def index(request):
 #         for i in range(0, num):
 #             res.append(arglist[i])
 #         finalres = []
-#
+
+
 #         class mytype:
 #             def __init__(self, res_id, res_file_name, res_url):
 #                 self.id = res_id
 #                 self.file_name = res_file_name
-#                 self.url = res_url  # 定义一个新的类型
-#
+#                 self.url = res_url     # 定义一个新的类型
+
+
 #         for i in res:
 #             res_id = i
 #             res_file_name = namelist[i].split('/')[-1]
 #             res_url = '.' + namelist[i][36:]
 #             temp = mytype(res_id, res_file_name, res_url)
-#             # finalres列表里装的就是自定义对象
-#             finalres.append(temp)
-#         # 这里修改后会返回一个类
+#             finalres.append(temp)  # finalres列表里装的就是自定义对象
 #         return JsonResponse({"status": 0, "data": finalres})
